1f39cc0a4ea0e0d6f7d9c71c4a33db3b951a2385
yuko fuyutsuki <yukofuyutsuki@gmail.com>
2022-09-08 00:25:41 +0900

    欠落した型宣言を追加

--- src/oirancage/libcamera/EventHandler.php
+++ src/oirancage/libcamera/EventHandler.php
@@ -6,9 +6,9 @@ use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\InteractPacket;
 
-class EventHandler implements Listener
-{
-    public function onDataPacketReceive(DataPacketReceiveEvent $event){
+class EventHandler implements Listener{
+
+    public function onDataPacketReceive(DataPacketReceiveEvent $event): void{
         $pk = $event->getPacket();
         if($pk instanceof InteractPacket && $pk->action === InteractPacket::ACTION_LEAVE_VEHICLE){
             libCamera::getInstance()
